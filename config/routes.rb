ecb203a2d105a2e39a0888f18ab3779f32918718
Mitchell Van Braeckel <mvanbrae@uoguelph.ca>
2021-10-23 15:01:30 -0400

    set routes for handling logins and logouts, changed root route from hello rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
+  root to: "home#index"
+
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
+
+  get "signup", to: "users#new", as: "signup"
+  get "login", to: "sessions#new", as: "login"
+  get "logout", to: "sessions#destroy", as: "logout"
+
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
-  resources :users
   get 'home/index'
-  root "articles#index"
+
+  # root "articles#index"
 
   get "/articles", to: "articles#index"
 
