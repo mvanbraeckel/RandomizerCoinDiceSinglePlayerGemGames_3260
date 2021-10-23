87ca9220486c05ea07e1a0f21243f3b3ae15fe83
Mitchell Van Braeckel <mvanbrae@uoguelph.ca>
2021-10-23 19:42:12 -0400

    attempting to 0 points and gems when creating new user in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
+    @user = User.new(user_params, points: params[:points], gems: params[:gems])
 
     respond_to do |format|
       if @user.save
