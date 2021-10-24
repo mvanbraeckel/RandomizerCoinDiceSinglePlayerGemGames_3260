dc295b96c9249f41b51bb7953c7cbe4538d1b34a
mvanbraeckel <mvanbrae@uoguelph.ca>
2021-10-23 20:49:26 -0400

    rails db migrate after generating migration for points and gems on users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_23_210724) do
+ActiveRecord::Schema.define(version: 2021_10_23_234317) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2021_10_23_210724) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
+    t.integer "points"
+    t.integer "gems"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
