06425c07f31da266d3d4c21f2ed9153b54246101
mvanbraeckel <mvanbrae@uoguelph.ca>
2021-10-24 15:10:31 -0400

    migrate db after item creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_23_234317) do
+ActiveRecord::Schema.define(version: 2021_10_24_183952) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "item"
+    t.float "denomination"
+    t.integer "sides"
+    t.string "colour"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -23,4 +34,5 @@ ActiveRecord::Schema.define(version: 2021_10_23_234317) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
