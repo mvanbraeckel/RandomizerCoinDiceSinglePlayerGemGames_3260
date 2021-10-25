fba670307c327a30683141e2a6ac804d3fd36a71
Mitchell Van Braeckel <mvanbrae@uoguelph.ca>
2021-10-25 16:05:48 -0400

    fixing game to games routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :items
   end
   resources :purchase_items
-  resource :game
+  resource :games
 
   get "playgame", to: "games#index", as: "playgame"
   # get "showgame", to: "games#show", as: "showgame"
