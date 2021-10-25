83f85d724d474a6e19c3e1a74a23a1f6e0e5d04a
Mitchell Van Braeckel <mvanbrae@uoguelph.ca>
2021-10-25 03:31:04 -0400

    trying to fix denomination validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,15 +5,15 @@ class Item < ApplicationRecord
 
   with_options if: :is_coin? do |coin|
     coin.validates :denomination, presence: true, numericality: { greater_than: 0.0 },
-      inclusion: { in: %w(0.05 0.1 0.10 0.25 1 1.0 1.00 2 2.0 2.00), message: "value '%{value}' is not a valid coin denomination" }
+      inclusion: { in: %w(0.05 0.1 0.10 0.25 1 1.0 1.00 2 2.0 2.00), message: "value '%{value}' is not a valid coin denomination" }, allow_blank: false
     coin.validates :sides, presence: false, allow_blank: true
     coin.validates :colour, presence: false, allow_blank: true
   end
 
   with_options if: :is_die? do |die|
     die.validates :denomination, presence: false, allow_blank: true
-    die.validates :sides, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
-    die.validates :colour, presence: true, inclusion: { in: %w(red, green, blue, yellow, black, white), message: "value '%{value}' is not a valid die colour" }
+    die.validates :sides, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }, allow_blank: false
+    die.validates :colour, presence: true, inclusion: { in: %w(red, green, blue, yellow, black, white), message: "value '%{value}' is not a valid die colour" }, allow_blank: false
   end
 
   def is_coin?
