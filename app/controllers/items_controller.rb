2dce185d9b476ceae02fe77d8203d0f94ef1b646
Mitchell Van Braeckel <mvanbrae@uoguelph.ca>
2021-10-25 03:29:55 -0400

    fixing detroy redirect path

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -43,7 +43,7 @@ class ItemsController < ApplicationController
   def update
     respond_to do |format|
       if @item.update(item_params)
-        format.html { redirect_to @item, notice: 'Item was successfully updated.' }
+        format.html { redirect_to user_item_path(@user, @item), notice: 'Item was successfully updated.' }
         format.json { render :show, status: :ok, location: @item }
       else
         format.html { render :edit }
@@ -58,7 +58,7 @@ class ItemsController < ApplicationController
     # @item = @user.items.find(params[:id])
     @item.destroy
     respond_to do |format|
-      format.html { redirect_to items_url, notice: 'Item was successfully destroyed.' }
+      format.html { redirect_to user_items_path(@user), notice: 'Item was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
