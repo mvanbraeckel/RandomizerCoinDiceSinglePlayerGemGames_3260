a590a06bb7ddbe3f8be4a8b51118248182f7480f
Mitchell Van Braeckel <mvanbrae@uoguelph.ca>
2021-10-25 18:05:46 -0400

    add alerts saying it inapplicable parms were ignored

--- app/controllers/purchase_items_controller.rb
+++ app/controllers/purchase_items_controller.rb
@@ -25,17 +25,17 @@ class PurchaseItemsController < ApplicationController
     #   @item = current_user.items.create(item: params[:item], sides: params[:sides], colour: params[:colour])
     # end
 
-    alert_text = ""
+    alert_text = "Purchase alert: "
     if @item.item == "coin" || @item.item == :coin
       if @item.sides || @item.sides != 0
-        alert_text += "Purchase alert: ignored number of sides '#{@item.sides}' when purchasing the coin.\n"
+        alert_text += "Ignored number of sides '#{@item.sides}' when purchasing the coin. "
       elsif @item.colour
-        alert_text += "Purchase alert: ignored colour '#{@item.colour}' when purchasing the coin.\n"
+        alert_text += "Ignored colour '#{@item.colour}' when purchasing the coin. "
       end
       @item = current_user.items.create(purchase_coin_params)
     elsif @item.item == "die" || @item.item == :die
       if @item.denomination
-        alert_text += "Purchase alert: ignored denomination '#{@item.denomination}' when purchasing the die.\n"
+        alert_text += "Ignored denomination '#{@item.denomination}' when purchasing the die. "
       end
       item_cost = @item.sides
       @item = current_user.items.create(purchase_die_params)
@@ -44,11 +44,11 @@ class PurchaseItemsController < ApplicationController
     # @item = current_user.items.create(purchase_item_params)
 
     if !item_cost
-      alert_text += "Purchase refused. Please input a number of sides for the die you want to purchase."
+      alert_text += "Purchase refused - Please input a number of sides for the die you want to purchase."
       flash.now[:alert] = "#{alert_text}"
       render :new
     elsif current_user.gems < item_cost
-      alert_text += "Purchase refused. You don't have enough gems. The item costs #{item_cost}, but you only have #{current_user.gems} gems."
+      alert_text += "Purchase refused - You don't have enough gems. The item costs #{item_cost}, but you only have #{current_user.gems} gems."
       flash.now[:alert] = "#{alert_text}"
       render :new
     else
