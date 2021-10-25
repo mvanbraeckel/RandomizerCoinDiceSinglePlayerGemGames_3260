e41e7df541f8e66631aba3728c702078327e22b6
Mitchell Van Braeckel <mvanbrae@uoguelph.ca>
2021-10-25 14:10:46 -0400

    trying to get validation working with select boxes and gem cost buy

--- app/controllers/purchase_items_controller.rb
+++ app/controllers/purchase_items_controller.rb
@@ -23,19 +23,20 @@ class PurchaseItemsController < ApplicationController
 
     @item = current_user.items.create(purchase_item_params)
 
-    respond_to do |format|
-      if current_user.save && @item.save
-        if current_user.gems < item_cost
-          flash.now[:alert] = "Purchase refused. You don't have enough gems. The item costs #{item_cost}, but you only have #{current_user.gems} gems."
-          render :new
-        else
-          current_user.gems -= item_cost
+    if current_user.gems < item_cost
+      flash.now[:alert] = "Purchase refused. You don't have enough gems. The item costs #{item_cost}, but you only have #{current_user.gems} gems."
+      render :new
+    else
+      current_user.gems -= item_cost
+
+      respond_to do |format|
+        if current_user.save && @item.save
           format.html { redirect_to purchase_item_path(@item), notice: 'Item was successfully purchased.' }
           format.json { render :show, status: :created, location: @item }
+        else
+          format.html { render :new }
+          format.json { render json: @item.errors, status: :unprocessable_entity }
         end
-      else
-        format.html { render :new }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
       end
     end
   end
