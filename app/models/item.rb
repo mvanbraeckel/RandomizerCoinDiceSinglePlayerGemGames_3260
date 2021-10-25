bd7e58397d2acb18b486acc8a28922390a4aa9e7
Mitchell Van Braeckel <mvanbrae@uoguelph.ca>
2021-10-25 03:47:03 -0400

    trying to fix denomination validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
   validates :item, inclusion: { in: %w(coin die), message: "type '%{value}' is not a valid item - must be 'coin' or 'die'" }
 
   with_options if: :is_coin? do |coin|
-    coin.validates :denomination, presence: true, format: { with: /\A(0.05|0.1|0.10|0.25|1|1.0|1.00|2|2.0|2.00)\z/,
+    coin.validates :denomination, presence: true, numericality: { greater_than: 0.0 }, format: { with: /\A(0.05|0.1|0.10|0.25|1|1.0|1.00|2|2.0|2.00)\z/,
       message: "value '%{value}' is not a valid coin denomination." }
     coin.validates :sides, presence: false, allow_blank: true
     coin.validates :colour, presence: false, allow_blank: true
