1285ddb44f4601a0ea6745b977c7ad6faaa4f473
Mitchell Van Braeckel <mvanbrae@uoguelph.ca>
2021-10-25 04:18:17 -0400

    trying to fix denomination validation by inclusion of float set, try adding back custom message revert

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,8 @@ class Item < ApplicationRecord
   validates :item, inclusion: { in: %w(coin die), message: "type '%{value}' is not a valid item - must be 'coin' or 'die'" }
 
   with_options if: :is_coin? do |coin|
-    coin.validates :denomination, presence: true, numericality: { greater_than: 0.0 }, inclusion: [0.05 0.1 0.10 0.25 1 1.0 1.00 2 2.0 2.00]
+    coin.validates :denomination, presence: true, numericality: { greater_than: 0.0 }, inclusion: [0.05,0.1,0.10,0.25,1,1.0,1.00,2,2.0,2.00]
+    # todo -mvb- better error msg for invalid denomination
     coin.validates :sides, presence: false, allow_blank: true
     coin.validates :colour, presence: false, allow_blank: true
   end
