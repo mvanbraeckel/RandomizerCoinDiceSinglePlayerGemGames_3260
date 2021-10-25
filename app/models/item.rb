a5e9a0703078557eb22070dcdc33f6750862cb17
Mitchell Van Braeckel <mvanbrae@uoguelph.ca>
2021-10-25 02:38:37 -0400

    adding coin validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,12 +4,12 @@ class Item < ApplicationRecord
   validates :item, inclusion: { in: %w(coin die),
     message: "type '%{value}' is not a valid item - must be 'coin' or 'die'" }
 
-  # with_options if: :item == :coin do |coin|
-  #   coin.validates :denomination, presence: true, numericality: { greater_than: 0.0 }, inclusion: { in: %w(.05 0.05 .1 0.1 .10 0.10 .25 0.25 1 1.0 1.00 2 2.0 2.00),
-  #     message: "%{value} is not a valid coin denomination" }
-  #   coin.validates :sides, presence: false, allow_blank: true
-  #   coin.validates :colour, presence: false, allow_blank: true
-  # end
+  with_options if: :item == :coin do |coin|
+    coin.validates :denomination, presence: true, numericality: { greater_than: 0.0 }, inclusion: { in: %w(.05 0.05 .1 0.1 .10 0.10 .25 0.25 1 1.0 1.00 2 2.0 2.00),
+      message: "%{value} is not a valid coin denomination" }
+    coin.validates :sides, presence: false, allow_blank: true
+    coin.validates :colour, presence: false, allow_blank: true
+  end
 
   # with_options if: :item == :die do |die|
   #   die.validates :denomination, presence: false, allow_blank: true
